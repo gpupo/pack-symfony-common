9393aaade33587bba4cfa525c100ee5fffad1cfc
Gilmar Pupo <contact@gpupo.com>
2020-10-20 09:48:31 -0300

    Apply CS fixer

--- src/Amqp/Formatter.php
+++ src/Amqp/Formatter.php
@@ -3,16 +3,9 @@
 declare(strict_types=1);
 
 /*
- * This file is part of gpupo/pack-symfony-common
- * Created by Gilmar Pupo <contact@gpupo.com>
- * For the information of copyright and license you should read the file
- * LICENSE which is distributed with this source code.
- * Para a informação dos direitos autorais e de licença você deve ler o arquivo
- * LICENSE que é distribuído com este código-fonte.
- * Para obtener la información de los derechos de autor y la licencia debe leer
- * el archivo LICENSE que se distribuye con el código fuente.
- * For more information, see <https://opensource.gpupo.com/>.
- *
+ * This file is part of gpupo/pack-symfony-common created by Gilmar Pupo <contact@gpupo.com>
+ * For the information of copyright and license you should read the file LICENSE which is
+ * distributed with this source code. For more information, see <https://opensource.gpupo.com/>
  */
 
 namespace Gpupo\PackSymfonyCommon\Amqp;
@@ -29,10 +22,10 @@ class Formatter extends JsonFormatter
     public function format(array $record): string
     {
         $data = $this->normalize($record);
-        if (isset($data['context']) && $data['context'] === []) {
+        if (isset($data['context']) && [] === $data['context']) {
             $data['context'] = new \stdClass();
         }
-        if (isset($data['extra']) && $data['extra'] === []) {
+        if (isset($data['extra']) && [] === $data['extra']) {
             $data['extra'] = new \stdClass();
         }
 
